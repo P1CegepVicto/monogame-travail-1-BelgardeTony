b304bdd25c03a9ad9d0448742ff4c8dde17e6f9a
Exsotix <belgarde.tony@carrefour.cegepvicto.ca>
2016-11-11 15:59:06 -0500

    3e envoye
    11/11/2016
    J'ai un Trump que nous pouvons contrôler de gauche à droite et qui lance
    des briques, une Hillary qui bouge de gauche a droite seul plus vite que
    Trump et qui lance des piments mexicain, j'ai des image qui apparait
    lorsque les projectiles touche leurs enemis et la partie fini. Il me
    reste a faire en sorte que le jeu puisse avoir plusieurs niveau de jeux
    et qui compte les points.

--- Projet_MonoGame/Projet_01/Game1.cs
+++ Projet_MonoGame/Projet_01/Game1.cs
@@ -12,11 +12,16 @@ namespace Projet_01
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Rectangle fenetre;
+
         Texture2D backgroundTexture;
+        Texture2D HillaryWin;
+        Texture2D TrumpWin;
 
         GameObject Hillary;
         GameObject Trump;
         GameObject brique;
+        GameObject mexican;
+        
 
         public Game1()
         {
@@ -37,7 +42,7 @@ namespace Projet_01
             this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
             this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
             this.graphics.ToggleFullScreen();
-            fenetre = new Rectangle(0, 0,graphics.GraphicsDevice.DisplayMode.Width,graphics.GraphicsDevice.DisplayMode.Height);
+            fenetre = new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height);
             base.Initialize();
         }
 
@@ -51,33 +56,39 @@ namespace Projet_01
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Trump = new GameObject();
             Trump.estVivant = true;
-            
+
             Hillary = new GameObject();
             Hillary.estVivant = true;
 
             brique = new GameObject();
             brique.estVivant = false;
 
+            mexican = new GameObject();
+            mexican.estVivant = false;
+
             //***********************************************************
 
             backgroundTexture = Content.Load<Texture2D>("backgroundTexture.png");
+            HillaryWin = Content.Load<Texture2D>("HillaryWin.png");
+            TrumpWin = Content.Load<Texture2D>("TrumpWin.png");
 
             //Limite à gauche de X = -10
             //Limite à droite de X = 1740
             //Limite en hauteur de Y = -15
             //Limite en bas de Y = 885 
             Trump.position.X = 820;
-            Trump.position.Y = 400;
+            Trump.position.Y = 850;
             Trump.sprite = Content.Load<Texture2D>("Trump.png");
 
             brique.sprite = Content.Load<Texture2D>("brique.png");
+            mexican.sprite = Content.Load<Texture2D>("Mexican.png");
 
             Hillary.position.X = 820;
             Hillary.position.Y = 15;
             Hillary.sprite = Content.Load<Texture2D>("Hillary.png");
-            Hillary.vitesse.X = 3;
+            Hillary.vitesse.X = 10;
 
-            
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -99,10 +110,10 @@ namespace Projet_01
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            //********Trump controle********************
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
-                Trump.position.X -= 5;
+                Trump.position.X -= 10;
                 if (Trump.position.X < -10)
                 {
                     Trump.position.X = 1740;
@@ -110,45 +121,71 @@ namespace Projet_01
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
-                Trump.position.X += 5;
+                Trump.position.X += 10;
                 if (Trump.position.X > 1740)
                 {
                     Trump.position.X = -10;
                 }
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            //if (Keyboard.GetState().IsKeyDown(Keys.S))
+            //{
+            //    Trump.position.Y += 5;
+            //    if (Trump.position.Y > 885)
+            //    {
+            //        Trump.position.Y = -15;
+            //    }
+            //}
+            //if (Keyboard.GetState().IsKeyDown(Keys.W))
+            //{
+            //    Trump.position.Y -= 5;
+            //    if (Trump.position.Y < -15)
+            //    {
+            //        Trump.position.Y = 885;
+            //    }
+
+            //}
+
+            //************Projectile controle***********************
+            if (brique.estVivant == false)
             {
-                Trump.position.Y += 5;
-                if (Trump.position.Y > 885)
+                if (Keyboard.GetState().IsKeyDown(Keys.Space))
                 {
-                    Trump.position.Y = -15;
+                    brique.estVivant = true;
+                    brique.position.X = Trump.position.X + 45;
+                    brique.position.Y = Trump.position.Y - 100;
                 }
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
-                Trump.position.Y -= 5;
-                if (Trump.position.Y < -15)
-                {
-                    Trump.position.Y = 885;
-                }
 
+            if (mexican.estVivant == false)
+            {
+                //if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                //{
+                //    mexican.estVivant = true;
+                //    mexican.position.X = Hillary.position.X + 50;
+                //    mexican.position.Y = Hillary.position.Y + 165;
+                //}
+                mexican.estVivant = true;
+                mexican.position.X = Hillary.position.X + 50;
+                mexican.position.Y = Hillary.position.Y + 165;
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Space))
-            {
-                brique.estVivant = true;
-                brique.position.X = Trump.position.X + 45;
-                brique.position.Y = Trump.position.Y -100;    
-            }
-                //*****************Hillary controle*************
+            //*****************Hillary controle********************
             //if (Keyboard.GetState().IsKeyDown(Keys.Left))
             //{
             //    Hillary.position.X -= 5;
+            //    if (Hillary.position.X < -10)
+            //    {
+            //        Hillary.position.X = 1740;
+            //    }
             //}
 
             //if (Keyboard.GetState().IsKeyDown(Keys.Right))
             //{
             //    Hillary.position.X += 5;
+            //    if (Hillary.position.X > 1740)
+            //    {
+            //        Hillary.position.X = -10;
+            //    }
             //}
 
             //if (Keyboard.GetState().IsKeyDown(Keys.Down))
@@ -163,28 +200,45 @@ namespace Projet_01
 
             // TODO: Add your update logic here
             UpdateHillary();
-            Updatebrique();
+            Updateprojectil();
             base.Update(gameTime);
         }
-        protected void Updatebrique()
+        protected void Updateprojectil()
         {
-            brique.position.Y -= 6; 
+            brique.position.Y -= 10;
+            mexican.position.Y += 20;
+            if (mexican.position.Y >= 885)
+            {
+                mexican.estVivant = false;
+            }
+            if (brique.position.Y <= -15)
+            {
+                brique.estVivant = false;
+            }
         }
 
         protected void UpdateHillary()
         {
-            
+            if (Hillary.GetRect().Intersects(brique.GetRect()))
+            {
+                Hillary.estVivant = false;
+            }
+            if (Trump.GetRect().Intersects(mexican.GetRect()))
+            {
+                Trump.estVivant = false;
+            }
+
 
             if (Hillary.position.X < fenetre.Left)
             {
-                Hillary.vitesse.X = 3;
+                Hillary.vitesse.X = 10;
             }
-            
-            if(Hillary.position.X + Hillary.sprite.Bounds.Width > fenetre.Right)
+
+            if (Hillary.position.X + Hillary.sprite.Bounds.Width > fenetre.Right)
             {
-                Hillary.vitesse.X = -3;
+                Hillary.vitesse.X = -10;
             }
-            
+
             Hillary.position += Hillary.vitesse;
         }
 
@@ -194,28 +248,57 @@ namespace Projet_01
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
-
-            spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height), Color.White);
-
-            spriteBatch.Draw(Trump.sprite, Trump.position, Color.White);
-
-            spriteBatch.Draw(Hillary.sprite, Hillary.position, Color.IndianRed);
-            if (brique.estVivant == true)
+            if (Hillary.estVivant == false)
             {
-                spriteBatch.Draw(brique.sprite, brique.position, Color.White);
+                //GraphicsDevice.Clear(Color.DarkOrange);
+
+                spriteBatch.Begin();
+
+                spriteBatch.Draw(TrumpWin, new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height), Color.Orange);
+
+                spriteBatch.End();
+            }
+            else if (Trump.estVivant == false)
+            {
+                //GraphicsDevice.Clear(Color.DarkRed);
+
+                spriteBatch.Begin();
+
+                spriteBatch.Draw(HillaryWin, new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height), Color.Red);
+
+                spriteBatch.End();
             }
 
+            if (Hillary.estVivant == true && Trump.estVivant == true)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+                spriteBatch.Begin();
+
+
+                spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height), Color.White);
+
+                spriteBatch.Draw(Trump.sprite, Trump.position, Color.Orange);
+
+                spriteBatch.Draw(Hillary.sprite, Hillary.position, Color.IndianRed);
 
 
 
-            spriteBatch.End();
-
-
+                if (brique.estVivant == true)
+                {
+                    spriteBatch.Draw(brique.sprite, brique.position, Color.White);
+                }
+                if (mexican.estVivant == true)
+                {
+                    spriteBatch.Draw(mexican.sprite, mexican.position, Color.White);
+                }
+                spriteBatch.End();
+            }
+           
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+            
+          
         }
     }
 }
