041a0a818408d362f00259ae501a1bf43a3f4a69
Exsotix <belgarde.tony@carrefour.cegepvicto.ca>
2016-11-22 17:54:04 -0500

    Naruto 2
    22/11/2016
    J'ai mes 2 personnages, j'ai mon ennemie aléatoire, j'ai mon projectile
    pour les héros.
    Il me manque que l'ennemie tire des projectiles aléatoire et lorsqu'ils
    sont toucher qu'ils meurent.

--- Projet_MonoGame/Projet_2-Naruto/Game1.cs
+++ Projet_MonoGame/Projet_2-Naruto/Game1.cs
@@ -15,10 +15,10 @@ namespace Projet_2_Naruto
         Rectangle fenetre;
         Texture2D background;
         Random random = new Random();
+        int rotate = 0;
 
         GameObject Naruto;
-        //GameObject[] Madara;
-        GameObject Madara;
+        GameObject[] Madara;
         GameObject Kunai;
         GameObject Shuriken;
 
@@ -61,13 +61,6 @@ namespace Projet_2_Naruto
             Naruto.position.X = 0;
             Naruto.position.Y = 0;
 
-            //***********Besoin d'aide*************************************
-            //Madara = new GameObject[12];
-            //for (int i = 0; i < Madara.Length; i++)
-            //{
-            //    Madara[i].sprite = Content.Load<Texture2D>("Madara.png");
-            //}
-            //*************************************************************
 
             Kunai = new GameObject();
             Kunai.estVivant = false;
@@ -78,6 +71,20 @@ namespace Projet_2_Naruto
             Shuriken.sprite = Content.Load<Texture2D>("Shuriken.png");
 
             background = Content.Load<Texture2D>("Background.png");
+
+            Madara = new GameObject[5];
+
+            for (int i = 0; i < Madara.Length; i++)
+            {
+                Madara[i] = new GameObject();
+                Madara[i].estVivant = true;
+                Madara[i].position.X = 1600;
+                Madara[i].position.Y = 300;
+                Madara[i].vitesse.X = random.Next(3, 9);
+                Madara[i].vitesse.Y = random.Next(3, 9);
+                Madara[i].sprite = Content.Load<Texture2D>("Madara.png");
+
+            }
 
             //******************************
             //Limite à gauche de X = 0
@@ -110,20 +117,21 @@ namespace Projet_2_Naruto
 
             // TODO: Add your update logic here
 
-            UpdateNaruto();
-            UpdateMadara();
+            rotate -= 1;
+            MoveNaruto();
+            MoveMadara();
             UpdateProjectile();
             base.Update(gameTime);
         }
 
-        protected void UpdateNaruto()
+        protected void MoveNaruto()
         {
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
                 Naruto.position.X -= 10;
                 if (Naruto.position.X < 0)
                 {
-                    Naruto.position.X = 1720;
+                    Naruto.position.X = 0;
                 }
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
@@ -131,7 +139,7 @@ namespace Projet_2_Naruto
                 Naruto.position.X += 10;
                 if (Naruto.position.X > 1720)
                 {
-                    Naruto.position.X = 0;
+                    Naruto.position.X = 1720;
                 }
             }
             if (Keyboard.GetState().IsKeyDown(Keys.S))
@@ -139,7 +147,7 @@ namespace Projet_2_Naruto
                 Naruto.position.Y += 10;
                 if (Naruto.position.Y > 945)
                 {
-                    Naruto.position.Y = 0;
+                    Naruto.position.Y = 945;
                 }
             }
             if (Keyboard.GetState().IsKeyDown(Keys.W))
@@ -147,20 +155,97 @@ namespace Projet_2_Naruto
                 Naruto.position.Y -= 10;
                 if (Naruto.position.Y < 0)
                 {
-                    Naruto.position.Y = 945;
+                    Naruto.position.Y = 0;
                 }
 
             }
+            if (Shuriken.estVivant == false && Kunai.estVivant == false)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                {
+                    Shuriken.estVivant = true;
+                    Shuriken.position.X = Naruto.position.X + 200 ;
+                    Shuriken.position.Y = Naruto.position.Y + 50;
+                }
+            }
+
+            if (Kunai.estVivant == false && Shuriken.estVivant == false)
+            {
+                if(Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
+                    Kunai.estVivant = true;
+                    Kunai.position.X = Naruto.position.X + 200;
+                    Kunai.position.Y = Naruto.position.Y + 50;
+                }
+            }
         }
 
-        protected void UpdateMadara()
+        protected void MoveMadara()
         {
+            for (int i = Madara.Length - 1; i >= 0; i--)
+            {
 
+
+                Madara[i].position += Madara[i].vitesse;
+
+                if (Madara[i].position.X < fenetre.Left)
+                {
+                    Madara[i].position.X = fenetre.Left;
+                }
+                if (Madara[i].position.Y < fenetre.Top)
+                {
+                    Madara[i].position.Y = fenetre.Top;
+                }
+                if (Madara[i].position.X + Madara[i].sprite.Bounds.Width > fenetre.Right)
+                {
+                    Madara[i].position.X = fenetre.Right - Madara[i].sprite.Bounds.Width;
+                }
+                if (Madara[i].position.Y + Madara[i].sprite.Bounds.Height > fenetre.Bottom)
+                {
+                    Madara[i].position.Y = fenetre.Bottom - Madara[i].sprite.Bounds.Height;
+                }
+                if (Madara[i].position.X > fenetre.Right)
+                {
+                    Madara[i].position.X = fenetre.Right;
+                }
+                if (Madara[i].position.Y == fenetre.Top)
+                {
+                    Madara[i].vitesse.Y = random.Next(3, 12);
+                }
+
+                if (Madara[i].position.Y + Madara[i].sprite.Bounds.Height == fenetre.Bottom)
+                {
+                    Madara[i].vitesse.Y = random.Next(-12, -3);
+                }
+
+                if (Madara[i].position.X + Madara[i].sprite.Bounds.Width == fenetre.Right)
+                {
+                    Madara[i].vitesse.X = random.Next(-12, -3);
+                }
+
+                if (Madara[i].position.X <= fenetre.Left)
+                {
+                    Madara[i].vitesse.X = random.Next(3, 12);
+                }
+            }
         }
 
         protected void UpdateProjectile()
         {
+           
+                Shuriken.position.X += 20;
+           
+                Kunai.position.X += 20;
+            
+                if (Shuriken.position.X >= 1920)
+            {
+                Shuriken.estVivant = false;
+            }
 
+            if (Kunai.position.X >= 1920)
+            {
+                Kunai.estVivant = false;
+            }
         }
 
         /// <summary>
@@ -174,8 +259,18 @@ namespace Projet_2_Naruto
 
             spriteBatch.Draw(background, new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height), Color.White);
             spriteBatch.Draw(Naruto.sprite, Naruto.position, Color.White);
-            //spriteBatch.Draw(Madara.sprite, Madara.position, Color.White);
-
+            for (int i = 0; i < Madara.Length; i++)
+            {
+                spriteBatch.Draw(Madara[i].sprite, Madara[i].position);
+            }
+            if (Shuriken.estVivant == true)
+            {
+                spriteBatch.Draw(Shuriken.sprite, Shuriken.position, origin: new Vector2(37/2, 38/2), rotation:rotate /3);
+            }
+            if (Kunai.estVivant == true)
+            {
+                spriteBatch.Draw(Kunai.sprite, Kunai.position);
+            }
 
 
 
